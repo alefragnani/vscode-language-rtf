69fe7c9c0a4eb2a58dcac2180547d361e138c632
Alessandro Fragnani <alefragnani@hotmail.com>
2022-03-17 21:49:11 -0300

    Add web support (full - with whats new)

--- src/whats-new/contentProvider.ts
+++ src/whats-new/contentProvider.ts
@@ -3,16 +3,16 @@
 *  Licensed under the MIT License. See License.md in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-import { ChangeLogItem, ChangeLogKind, Sponsor, ContentProvider, Header, Image, IssueKind } from "../../vscode-whats-new/src/ContentProvider";
+import { ChangeLogItem, ChangeLogKind, ContentProvider, Header, Image, IssueKind, SupportChannel, SocialMediaProvider } from "../../vscode-whats-new/src/ContentProvider";
 
-export class WhatsNewRTFContentProvider implements ContentProvider {
+export class RtfContentProvider implements ContentProvider {
 
-    provideHeader(logoUrl: string): Header {
+    public provideHeader(logoUrl: string): Header {
         return <Header>{logo: <Image> {src: logoUrl, height: 50, width: 50}, 
             message: `Edit your <b>RTF</b> files with <b>Syntax Highlighting</b> and <b>Bracket matching</b>`};
     }
 
-    provideChangeLog(): ChangeLogItem[] {
+    public provideChangeLog(): ChangeLogItem[] {
         const changeLog: ChangeLogItem[] = [];
 
         changeLog.push({ kind: ChangeLogKind.VERSION, detail: { releaseNumber: "2.4.0", releaseDate: "October 2021" } });
@@ -126,9 +126,27 @@ export class WhatsNewRTFContentProvider implements ContentProvider {
         return changeLog;
     }
 
-    provideSponsors(): Sponsor[] {
-        const sponsors: Sponsor[] = [];
-        return sponsors
+    public provideSupportChannels(): SupportChannel[] {
+        const supportChannels: SupportChannel[] = [];
+        supportChannels.push({
+            title: "Become a sponsor on Patreon",
+            link: "https://www.patreon.com/alefragnani",
+            message: "Become a Sponsor"
+        });
+        supportChannels.push({
+            title: "Donate via PayPal",
+            link: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=EP57F3B6FXKTU&lc=US&item_name=Alessandro%20Fragnani&item_number=vscode%20extensions&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted",
+            message: "Donate via PayPal"
+        });
+        return supportChannels;
     }
-   
+}
+
+export class RtfSocialMediaProvider implements SocialMediaProvider {
+    public provideSocialMedias() {
+        return [{
+            title: "Follow me on Twitter",
+            link: "https://www.twitter.com/alefragnani"
+        }];
+    }
 }
